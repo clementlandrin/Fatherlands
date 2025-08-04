e5bba1e363d293455094ef5d511ea9d72f79e5bb
clementlandrin <clement.landrin@hotmail.fr>
2025-08-04 19:30:27 +0200

    create player if no save.

--- src/st/GameState.hx
+++ src/st/GameState.hx
@@ -5,4 +5,11 @@ class GameState extends State {
 	
 	@:s public var curRoomId : String;
 	@:ignore public var curRoom : ent.Room;
+
+	//@:s public var knowledgeRoot : KnowledgeNode;
+
+	public function new() {
+		super();
+		player = new ent.Player();
+	}
 }
